1114c461c4c98ef283ec47d758c886c0d3bc0cf8
Álvaro Tapiador <alvarotapiador@gmail.com>
2024-07-23 11:07:43 +0200

    Actualizadas las claves en los environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseUrl: 'https://gateway.marvel.com/v1/public/',
-  publicKey: '9f765b49bdb4574c8872fc960f95086d',
-  privateKey: '254827ebd8fd7145294ab48e339b68cf32c23034',
+  publicKey: '',
+  privateKey: '',
 };
